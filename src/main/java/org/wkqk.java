d409060730622ae6f7d3543f957e3dee71134879
unye123 <leeyeun06@naver.com>
2025-06-10 23:28:53 +0900

    수정1

--- src/main/java/org/wkqk.java
+++ src/main/java/org/wkqk.java
@@ -1,4 +1,23 @@
-package org;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.*;
+import java.awt.Font;
 
 public class wkqk {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("자격증 홈페이지");
+        frame.setSize(1080, 720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel logoLabel = new JLabel("lee Certificate", SwingConstants.CENTER);
+        logoLabel.setFont(new Font("Serif", Font.BOLD, 24));
+
+        frame.add(logoLabel, BorderLayout.NORTH);
+
+        frame.add(logoLabel);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
