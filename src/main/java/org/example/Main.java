d8aca22c30c5f18f6bc52bfa51464c3803f95b7f
unye123 <leeyeun06@naver.com>
2025-06-10 16:57:58 +0900

    Re-add org folder as normal directory

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("GitHub Desktop 연동 테스트!");
+        System.out.println("커밋 트트!");
+    }
+}
