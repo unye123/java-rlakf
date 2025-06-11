23cee320107660be84c8810cfbdb8cbfed50ca9e
unye123 <leeyeun06@naver.com>
2025-06-11 11:27:34 +0900

    수정5

--- src/main/java/org/wkqk.java
+++ src/main/java/org/wkqk.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.Font;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class wkqk {
     public static void main(String[] args) {
@@ -16,8 +18,17 @@ public class wkqk {
         JLabel loginLabel = new JLabel("로그인");
         loginLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 
+        JLabel noticeLabel = new JLabel("공지사항");
+        noticeLabel.setFont(new Font("맑은 고딕", Font.BOLD, 14));
+
+        JTextArea noticeArea = new jtextArea(10, 50);
+        
+
         JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JPanel topPanel = new JPanel(new BorderLayout());
+
+        JPanel centerNoticePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        centerNoticePanel.add(noticeLabel);
 
         topPanel.add(centerPanel, BorderLayout.CENTER);
 
@@ -25,6 +36,7 @@ public class wkqk {
 
         topPanel.add(loginLabel, BorderLayout.EAST);
         frame.add(topPanel, BorderLayout.NORTH);
+        frame.add(centerNoticePanel, BorderLayout.CENTER);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
